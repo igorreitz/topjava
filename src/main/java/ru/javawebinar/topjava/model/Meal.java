fb62784f4ac7ac5d30ab9c79fd8c2fba5dce9132
igorreitz <rayts@ngs.ru>
2019-03-15 14:06:33 +0700

    Add cascade ddl

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -2,6 +2,8 @@ package ru.javawebinar.topjava.model;
 
 import javax.persistence.FetchType;
 import javax.persistence.ManyToOne;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 import org.hibernate.validator.constraints.Range;
 
 import javax.persistence.*;
@@ -43,6 +45,7 @@ public class Meal extends AbstractBaseEntity {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
     @NotNull
     private User user;
 
