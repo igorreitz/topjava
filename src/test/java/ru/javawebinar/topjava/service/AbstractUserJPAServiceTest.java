0bff7e279c04a0c6bed76474a6d2639ccf373a64
igorreitz <rayts@ngs.ru>
2019-03-28 12:14:08 +0700

    Make HW6.Some fixes

---
+++ src/test/java/ru/javawebinar/topjava/service/AbstractUserJPAServiceTest.java
@@ -0,0 +1,20 @@
+package ru.javawebinar.topjava.service;
+
+import org.junit.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
+import ru.javawebinar.topjava.repository.JpaUtil;
+
+public abstract class AbstractUserJPAServiceTest extends AbstractUserServiceTest {
+    @Autowired
+    protected JpaUtil jpaUtil;
+
+    @Autowired
+    private CacheManager cacheManager;
+
+    @Before
+    public void setUp() throws Exception {
+        cacheManager.getCache("users").clear();
+        jpaUtil.clear2ndLevelHibernateCache();
+    }
+}
